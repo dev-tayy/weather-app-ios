da44d3b4c3da9350f3bd6472c3300fde5319f845
iloveteajay <atytjani@gmail.com>
2023-02-04 17:41:59 +0100

    fix: status bar colour

--- weather-app/ContentView.swift
+++ weather-app/ContentView.swift
@@ -9,108 +9,117 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Color("primary").ignoresSafeArea()
-            Image("cloudBackground").opacity(0.07).padding([.bottom], 400)
-            
-            VStack {
-               Group {
-                    Spacer().frame(height: 15)
-                    HStack (alignment: .center , spacing: 145){
-                        Button (action: {
-                            print("love")
-                        }) {
-                           Text("Search for places").padding([.horizontal], 18)
-                                .padding([.vertical], 11)
-                                .background(Color("grey"))
-                                .foregroundColor(Color("hardWhite"))
-                                .fontWeight(.medium)
+        NavigationView {
+            ScrollView {
+                ZStack{
+                    Color("primary").ignoresSafeArea()
+                    
+                    Image("cloudBackground").opacity(0.07).padding([.bottom], 200)
+                    
+                    VStack {
+                       Group {
+                            Spacer().frame(height: 60)
+                            HStack (alignment: .center , spacing: 145){
+                                Button (action: {
+                                    print("love")
+                                }) {
+                                   Text("Search for places").padding([.horizontal], 18)
+                                        .padding([.vertical], 11)
+                                        .background(Color("grey"))
+                                        .foregroundColor(Color("hardWhite"))
+                                        .fontWeight(.medium)
+                                }
+                                
+                                Button(action: {}){
+                                    Image(systemName: "location.circle.fill")
+                                        .resizable()
+                                        .frame(width: 40, height: 40)
+                                        .foregroundColor(Color("grey"))
+                                        .background(Color("hardWhite"))
+                                        .clipShape(Circle())
+                                        .aspectRatio(contentMode: .fill)
+                                }
+                            }
+                           Spacer().frame(height: 76)
                         }
                         
-                        Button(action: {}){
-                            Image(systemName: "location.circle.fill")
+                        Group{
+                            Image(systemName: "cloud.drizzle.fill")
                                 .resizable()
-                                .frame(width: 40, height: 40)
-                                .foregroundColor(Color("grey"))
-                                .background(Color("hardWhite"))
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 150, height: 174)
+                                .foregroundColor(Color("hardWhite"))
                                 .clipShape(Circle())
-                                .aspectRatio(contentMode: .fill)
+                            
+                            Spacer().frame(height:10)
+                            
+                            HStack  (alignment: .firstTextBaseline){
+                                Text("15")
+                                    .font(.custom("Raleway-SemiBold",  size: 144))
+                                    .foregroundColor(Color("hardWhite"))
+                                
+                                Text("℃")
+                                    .font(.custom("Raleway-Bold",  size: 48))
+                                    .foregroundColor(Color("lightGrey"))
+                                    .fontWeight(.regular)
+                            }
+                            
+                            Spacer().frame(height: 20)
+                            
+                            Text("Shower")
+                                .font(.custom("Raleway-Regular",  size: 36))
+                                .foregroundColor(Color("lightGrey"))
+                                .fontWeight(.semibold)
                         }
-                    }
-                   Spacer().frame(height: 76)
-                }
-                
-                Group{
-                    Image(systemName: "cloud.drizzle.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 150, height: 174)
-                        .foregroundColor(Color("hardWhite"))
-                        .clipShape(Circle())
-                    
-                    Spacer().frame(height:10)
-                    
-                    HStack  (alignment: .firstTextBaseline){
-                        Text("15")
-                            .font(.custom("Raleway-SemiBold",  size: 144))
-                            .foregroundColor(Color("hardWhite"))
                         
-                        Text("℃")
-                            .font(.custom("Raleway-Bold",  size: 48))
-                            .foregroundColor(Color("lightGrey"))
-                            .fontWeight(.regular)
-                    }
-                    
-                    Spacer().frame(height: 20)
-                    
-                    Text("Shower")
-                        .font(.custom("Raleway-Regular",  size: 36))
-                        .foregroundColor(Color("lightGrey"))
-                        .fontWeight(.semibold)
-                }
-                
-                Spacer().frame(height: 48)
-                
-                HStack {
-                    
-                    Text("Today")
-                        .font(.custom("Raleway-Regular",  size: 18))
-                        .foregroundColor(Color("hardGrey"))
-                        .fontWeight(.semibold)
-                    
-                    Spacer().frame(width: 16)
-                    
-                    Text("•")
-                        .font(.custom("Raleway-Regular",  size: 18))
-                        .foregroundColor(Color("hardGrey"))
-                        .fontWeight(.semibold)
-                    
-                    Spacer().frame(width: 16)
-                    
-                    Text("Fri, 5 Jun")
-                        .font(.custom("Raleway-Regular",  size: 18))
-                        .foregroundColor(Color("hardGrey"))
-                        .fontWeight(.semibold)
-                }
-                
-                Spacer().frame(height: 33)
-                
-                
-                Label {
-                    Text("Helsinki")
-                        .font(.custom("Raleway-Regular",  size: 18))
-                        .foregroundColor(Color("hardGrey"))
-                        .fontWeight(.semibold)
-                } icon: {
-                    Image(systemName: "location.circle")
-                        .foregroundColor(Color("lightGrey"))
-                }
+                        Spacer().frame(height: 48)
+                        
+                        HStack {
+                            
+                            Text("Today")
+                                .font(.custom("Raleway-Regular",  size: 18))
+                                .foregroundColor(Color("hardGrey"))
+                                .fontWeight(.semibold)
+                            
+                            Spacer().frame(width: 16)
+                            
+                            Text("•")
+                                .font(.custom("Raleway-Regular",  size: 18))
+                                .foregroundColor(Color("hardGrey"))
+                                .fontWeight(.semibold)
+                            
+                            Spacer().frame(width: 16)
+                            
+                            Text("Fri, 5 Jun")
+                                .font(.custom("Raleway-Regular",  size: 18))
+                                .foregroundColor(Color("hardGrey"))
+                                .fontWeight(.semibold)
+                        }
+                        
+                        Spacer().frame(height: 33)
+                        
+                        
+                        Label {
+                            Text("Helsinki")
+                                .font(.custom("Raleway-Regular",  size: 18))
+                                .foregroundColor(Color("hardGrey"))
+                                .fontWeight(.semibold)
+                        } icon: {
+                            Image(systemName: "location.circle")
+                                .foregroundColor(Color("lightGrey"))
+                        }
 
-                 Spacer()
+                         Spacer()
+                        
+                     }
+                }
                 
-             }
-        }
-        
+            }  .toolbarBackground(.visible, for: .navigationBar)
+                .toolbarColorScheme(.light, for: .navigationBar)
+                .ignoresSafeArea()
+                .background(Color("primary"))
+            
+            }
     }
 }
 
